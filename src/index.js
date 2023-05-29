225813d23bb1169f6eed71e8d570a5928ffd35ab
Krushna chandra sahu <krushnachsahu089@gmail.com>
2023-05-29 12:24:15 +0530

    Commited

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const {City}= require('./models/index');
 
 const { PORT } = require('./config/serverConfig');
 
+const ApiRoutes = require('./routes/index');
+
 const CityRepository = require('./repository/city-repository')
 const setupAndStartServer = async ()=>{
     // create the express object 
@@ -13,6 +15,11 @@ const setupAndStartServer = async ()=>{
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended :true}));
+
+    app.use('/api' , ApiRoutes);
+
+
+
     app.listen(PORT , async ()=>{
         console.log(`Server started at ${PORT}`);
         // console.log(process);
