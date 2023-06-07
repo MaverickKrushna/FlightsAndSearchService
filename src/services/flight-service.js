1ffaca44dabe142e612e63534fa1ccdb1c092b18
Krushna chandra sahu <krushnachsahu089@gmail.com>
2023-06-08 00:49:01 +0530

    added flights model

---
+++ src/services/flight-service.js
@@ -0,0 +1,13 @@
+const FlightRepository = require('../repository/flight-repository');
+
+class FlightService{
+    async createFlight(data){
+        try{
+
+        }catch(error){
+            console.log("something wrong at service layer ");
+
+            throw {error};
+        }
+    }
+}
